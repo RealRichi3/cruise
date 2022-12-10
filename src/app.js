f13b840054165ff9d6c755e0dfb75dab2e974991
Richie <molunorichie@gmail.com>
2022-12-10 20:16:38 +0100

    add: db connection

---
+++ src/app.js
@@ -0,0 +1,28 @@
+
+const express = require("express");
+const morgan = require("morgan");
+const cors = require('cors');
+const cookieParser = require('cookie-parser')
+const errorHandler = require("./middlewares/error_handler");
+const app = express();
+
+
+// Middlewares
+if (process.env.NODE_ENV == 'dev') app.use(morgan('dev'));
+
+app.use(cors())
+app.use(cookieParser())
+app.use(express.json())
+
+// RouteS
+
+// Error handler middleware
+app.use(errorHandler)
+app.use((req, res, next) => {
+    res.status(404).json({
+        status: 'fail',
+        message: `Can't find ${req.originalUrl} on this server!`
+    })
+})
+
+module.exports = app;
