026bf9b7b41650dfc5e398d40c2360a24adbb016
Richie <molunorichie@gmail.com>
2022-12-11 13:30:38 +0100

    add: wallet model - wallet.model.js

--- src/models/wallet.model.js
+++ src/models/wallet.model.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose')
 const schema = mongoose.Schema
 
-const walletSchema = new schema({
-    balance: { type: Number, required: true, default: 0 },
-    user: { type: schema.Types.ObjectId, ref: 'User', required: true },
-    transactions: [{ type: schema.Types.ObjectId, ref: 'Transaction' }],
-}, { timestamps: true })
+const walletSchema = new schema(
+    {
+        balance: { type: Number, required: true, default: 0 },
+        user: { type: schema.Types.ObjectId, ref: 'User', required: true },
+        transactions: [{ type: schema.Types.ObjectId, ref: 'Transaction' }],
+    },
+    { timestamps: true }
+)
 
 const Wallet = mongoose.model('Wallet', walletSchema)
 
