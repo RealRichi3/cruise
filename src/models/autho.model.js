114e32c6b02f85425532c9d6f316519ed9965390
Richie <molunorichie@gmail.com>
2022-12-11 13:50:13 +0100

    add: autho model for permissions - autho.model.js

---
+++ src/models/autho.model.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+const schema = mongoose.Schema
+
+const authoSchema = new schema({
+    user: { type: schema.Types.ObjectId, ref: 'User', required: true },
+    role: { type: String, required: true, enum: ['admin', 'rider', 'user'] },
+    // Format: "module:action"
+    permissions: [{ type: String, required: true }],
+    // Format: "module:action"
+    restrictions: [{ type: String, required: true }],
+    createdAt: { type: Date, default: Date.now },
+})
+
+const Autho = mongoose.model('Autho', authoSchema)
+
+module.exports = Autho
