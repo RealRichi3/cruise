5ef510c18973329b23a51696aa999622d604ceb6
Richie <molunorichie@gmail.com>
2022-12-11 13:38:30 +0100

    add: schema for withdrawal request - wallet.model.js

--- src/models/wallet.model.js
+++ src/models/wallet.model.js
@@ -6,10 +6,27 @@ const walletSchema = new schema(
         balance: { type: Number, required: true, default: 0 },
         user: { type: schema.Types.ObjectId, ref: 'User', required: true },
         transactions: [{ type: schema.Types.ObjectId, ref: 'Transaction' }],
+        withdrawal_requests: [
+            { type: schema.Types.ObjectId, ref: 'WithdrawalRequest' },
+        ],
     },
     { timestamps: true }
 )
 
-const Wallet = mongoose.model('Wallet', walletSchema)
+const withdrawalRequestSchema = new schema({
+    rider: { type: schema.Types.ObjectId, ref: 'User', required: true },
+    amount: { type: Number, required: true },
+    status: {
+        type: String,
+        required: true,
+        enum: ['pending', 'approved', 'declined'],
+        default: 'pending',
+    },
+    transaction: { type: schema.Types.ObjectId, ref: 'Transaction' },
+    createdAt: { type: Date, default: Date.now },
+})
 
-module.exports = Wallet
+const Wallet = mongoose.model('Wallet', walletSchema)
+const WithdrawalRequest = mongoose.model('WithdrawalRequest', withdrawalSchema)
+
+module.exports = { Wallet, WithdrawalRequest }
