49833488a97f0e384b438404cb190b3dd70fdbb9
Richie <molunorichie@gmail.com>
2022-12-24 10:30:51 +0100

    add: expiry to blacklisted tokens - token.model.js

--- src/models/token.model.js
+++ src/models/token.model.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose')
 const schema = mongoose.Schema
-const { JWT_REFRESH_EXPIRES_IN } = require('../utils/config')
+const { JWT_REFRESH_EXP } = require('../utils/config')
 
 const blacklistedTokenSchema = new schema(
     {
         token: { type: String, required: true },
         createdAt: { type: Date, default: Date.now },
     },
-    { timestamps: true, expires: JWT_REFRESH_EXPIRES_IN }
+    { timestamps: true, expires: JWT_REFRESH_EXP }
 )
 
 const authCodeSchema = new schema(
@@ -17,7 +17,7 @@ const authCodeSchema = new schema(
         password_reset_code: { type: String },
         createdAt: { type: Date, default: Date.now },
     },
-    { timestamps: true, expires: JWT_REFRESH_EXPIRES_IN }
+    { timestamps: true, expires: JWT_REFRESH_EXP }
 )
 
 const AuthCode = mongoose.model('AuthCode', authCodeSchema)
