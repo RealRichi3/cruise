8d370ad45ff8a7991b114db265eff828c7290141
Richie <molunorichie@gmail.com>
2022-12-26 00:23:56 +0100

    add: user routes - user.route.js

---
+++ src/routes/user.route.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+
+const basicAuth = require('../middlewares/auth');
+const rbacMiddleware = require('../middlewares/rbac');
+
+const {
+    addUserAccount,
+    getUserAccount,
+    updateUserAccount,
+    deactivateUserAccount,
+    activateUserAccount,
+} = require('../controllers/user.controller');
+
+router.use(basicAuth(), rbacMiddleware(['superadmin']));
+
+router
+    .post('/add', addUserAccount)
+    .get('/get', getUserAccount)
+    .put('/update', updateUserAccount)
+    .put('/deactivate', deactivateUserAccount)
+    .put('/activate', activateUserAccount);
+
+module.exports = router;
