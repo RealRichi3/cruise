4897989659f2cb2fd82eba892cc0b8545b808bc5
Richie <molunorichie@gmail.com>
2022-12-27 00:37:13 +0100

    add: vehicle controllers - vehicle-controller.js

---
+++ src/controllers/vehicle.controller.js
@@ -0,0 +1,24 @@
+
+const addVehicle = async (req, res) => { }
+
+const getVehicleData = async (req, res) => { }
+
+const updateVehicleData = async (req, res) => { }
+
+const removeVehicle = async (req, res) => { }
+
+const getRidersVehicles = async (req, res) => { }
+
+const activateVehicle = async (req, res) => { }
+
+const deactivateVehicle = async (req, res) => { }
+
+module.exports = {
+    addVehicle,
+    getVehicleData,
+    updateVehicleData,
+    removeVehicle,
+    getRidersVehicles,
+    activateVehicle,
+    deactivateVehicle,
+}
