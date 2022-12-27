3de5167205aa8409e3dd3571024f671b9ebadd4a
Richie <molunorichie@gmail.com>
2022-12-27 01:49:17 +0100

    update route format - vehicle.route.js

--- src/routes/vehicle.route.js
+++ src/routes/vehicle.route.js
@@ -10,13 +10,17 @@ const {
     activateVehicle,
     deactivateVehicle,
 } = require('../controllers/vehicle.controller');
+const { basicAuth } = require('../middlewares/auth');
+const rbacMiddleware = require('../middlewares/rbac');
+
+router.use(basicAuth(), rbacMiddleware('rider'));
 
 router
     .post('/add', addVehicle)
-    .get('/get/:id', getVehicleData)
-    .put('/update/:id', updateVehicleData)
-    .delete('/remove/:id', removeVehicle)
-    .get('/get/riders-vehicles/:id', getRidersVehicles)
+    .get('/:id', getVehicleData)
+    .put('/:id', updateVehicleData)
+    .delete('/:id', removeVehicle)
+    .get('/riders-vehicles/:id', getRidersVehicles)
     .put('/activate/:id', activateVehicle)
     .put('/deactivate/:id', deactivateVehicle);
 
