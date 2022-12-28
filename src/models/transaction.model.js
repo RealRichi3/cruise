f60e5339d5241c0bc6826d71103112a3e41200f7
Richie <molunorichie@gmail.com>
2022-12-28 21:45:49 +0100

    add: validation hook and extra field to tansactionmodel - transaction.model.js

--- src/models/transaction.model.js
+++ src/models/transaction.model.js
@@ -1,13 +1,13 @@
-const { BadRequestError } = require('../utils/custom_errors')
+const { BadRequestError } = require('../utils/custom_errors');
 
 const mongoose = require('mongoose'),
     bcrypt = require('bcryptjs'),
     schema = mongoose.Schema,
-    UU = require('uuid').v4
+    UU = require('uuid').v4;
 
 const invoiceSchema = new schema({
     user: { type: schema.Types.ObjectId, ref: 'User', required: true },
-    invoiceId: { type: UU, required: true },
+    invoice_id: { type: String, required: true, default: new UU() },
     amount: { type: Number, required: true },
     type: {
         type: String,
@@ -21,26 +21,22 @@ const invoiceSchema = new schema({
     },
     date: { type: Date, default: Date.now },
     createdAt: { type: Date, default: Date.now },
-})
+});
 
 const receiptSchema = new schema({
     user: { type: schema.Types.ObjectId, ref: 'User', required: true },
     rider: { type: schema.Types.ObjectId, ref: 'Rider' },
+    enduser: { type: schema.Types.ObjectId, ref: 'EndUser' },
     ride: { type: schema.Types.ObjectId, ref: 'Ride' },
     amount: { type: Number, required: true },
-    type: {
-        type: String,
-        required: true,
-        enum: ['wallet_topup', 'book_ride', 'wallet_withdrawal'],
-    },
     transaction: {
         type: schema.Types.ObjectId,
         ref: 'Transaction',
         required: true,
     },
-    reference: { type: UU, required: true },
+    reference: { type: String, required: true },
     createdAt: { type: Date, default: Date.now },
-})
+});
 
 const transactionsSchema = new schema(
     {
@@ -56,10 +52,21 @@ const transactionsSchema = new schema(
             ref: 'Receipt',
             required: true,
         },
+        ride: { type: schema.Types.ObjectId, ref: 'Ride' },
         invoice: {
             type: schema.Types.ObjectId,
             ref: 'Invoice',
             required: true,
+        },
+        payment_method: {
+            type: String,
+            required: true,
+            enum: ['ussd', 'card', 'bank_transfer'],
+        },
+        type: {
+            type: String,
+            required: true,
+            enum: ['wallet_topup', 'book_ride', 'wallet_withdrawal'],
         },
         status: {
             type: String,
@@ -67,49 +74,64 @@ const transactionsSchema = new schema(
             default: 'pending',
             enum: ['pending', 'success', 'failed'],
         },
+        reference: { type: String, default: new UU() },
     },
     { timestamps: true }
-)
+);
 
 receiptSchema.pre('save', async function (next) {
-    const receipt = await this.populate('transaction').execPopulate()
+    const receipt = await this.populate('transaction').execPopulate();
 
     if (receipt.transaction.status != 'success') {
-        throw new BadRequestError('Receipt transaction is not successful')
+        throw new BadRequestError('Receipt transaction is not successful');
     }
 
     if (receipt.transaction.type != receipt.type) {
-        throw new BadRequestError('Receipt transaction type does not match')
+        throw new BadRequestError('Receipt transaction type does not match');
     }
 
     if (receipt.transaction.amount != receipt.amount) {
-        throw new BadRequestError('Receipt transaction amount does not match')
+        throw new BadRequestError('Receipt transaction amount does not match');
     }
 
     if (receipt.transaction.user != receipt.user) {
-        throw new BadRequestError('Receipt transaction user does not match')
+        throw new BadRequestError('Receipt transaction user does not match');
     }
 
     if (receipt.type == 'wallet_topup') {
         if (receipt.ride) {
-            throw new BadRequestError('Receipt type is wallet_topup')
+            throw new BadRequestError('Receipt type is wallet_topup');
         }
     }
 
     if (receipt.type == 'book_ride') {
         if (!receipt.ride) {
-            throw new BadRequestError('Please specify Ride id')
+            throw new BadRequestError('Please specify Ride id');
         }
         if (!receipt.rider) {
-            throw new BadRequestError('Please specify Rider id')
+            throw new BadRequestError('Please specify Rider id');
         }
     }
 
-    next()
-})
+    next();
+});
 
-const Invoice = mongoose.model('Invoice', invoiceSchema)
-const Receipt = mongoose.model('Receipt', receiptSchema)
-const Transaction = mongoose.model('Transaction', transactionsSchema)
+transactionsSchema.pre('validate', async function (next) {
+    return new Promise(async (resolve, reject) => {
+        if (this.status == 'success') {
+            // There should be a receipt if status is success
+            if (!this.receipt) {
+                return reject(new BadRequestError('Please specify receipt id'));
+            }
+            if (!this.invoice) {
+                return reject(new BadRequestError('Please specify invoice id'));
+            }
+        }
+    });
+});
 
-module.exports = { Invoice, Receipt, Transaction }
+const Invoice = mongoose.model('Invoice', invoiceSchema);
+const Receipt = mongoose.model('Receipt', receiptSchema);
+const Transaction = mongoose.model('Transaction', transactionsSchema);
+
+module.exports = { Invoice, Receipt, Transaction };
