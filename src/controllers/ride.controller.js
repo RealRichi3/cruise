9fe33a13e8ec55ed77857ed1830e91b3f88e504f
Richie <molunorichie@gmail.com>
2023-01-09 03:47:17 +0100

    add: dummy controls for fide feature - ride.controller.js

---
+++ src/controllers/ride.controller.js
@@ -0,0 +1,38 @@
+const bookRide = async (req, res) => {};
+
+const acceptRideRequest = async (req, res) => {};
+
+const declineRideRequest = async (req, res) => {};
+
+const cancelRide = async (req, res) => {};
+
+const startRide = async (req, res) => {};
+
+const completeRide = async (req, res) => {};
+
+const reviewRide = async (req, res) => {};
+
+const getRides = async (req, res) => {};
+
+const getRideData = async (req, res) => {};
+
+const getRideReviews = async (req, res) => {};
+
+const getRideReviewData = async (req, res) => {};
+
+const payForRide = async (req, res) => {};
+
+module.exports = {
+    bookRide,
+    acceptRideRequest,
+    declineRideRequest
+    cancelRide,
+    startRide,
+    completeRide,
+    reviewRide,
+    getRides,
+    getRideData,
+    getRideReviews,
+    getRideReviewData,
+    payForRide,
+};
