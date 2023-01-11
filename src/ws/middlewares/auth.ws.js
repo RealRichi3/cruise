50bcd90de7964f3665671e662ec5ef0d26b8b35c
Richie <molunorichie@gmail.com>
2023-01-11 16:57:14 +0100

    moved WebSocket middlewares to /ws/middlewares

---
+++ src/ws/middlewares/auth.ws.js
@@ -0,0 +1,26 @@
+const config = require('../../utils/config');
+const jwt = require('jsonwebtoken');
+
+// const wsVehicle = require('./ws/vehicle.ws');
+
+async function authMiddleware(socket, request) {
+    try {
+        const token = request.headers['sec-websocket-protocol'];
+        return await jwt.verify(
+            token,
+            config.JWT_ACCESS_SECRET,
+            (error, decoded) => {
+                if (error) {
+                    return error;
+                } else {
+                    socket.user = decoded;
+                    return socket;
+                }
+            }
+        );
+    } catch (error) {
+        console.log(error);
+        return error;
+    }
+}
+exports.authMiddleware = authMiddleware;
