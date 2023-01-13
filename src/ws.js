a840c152ea15e8973d219784446f0cbab27ea06c
Richie <molunorichie@gmail.com>
2023-01-13 15:31:04 +0100

    add: support for many clients - ws.js

--- src/ws.js
+++ src/ws.js
@@ -11,72 +11,65 @@ const wss = new WebSocketServer.Server({ server: express_server, path: '/ws' });
 const { RiderSockets } = require('./ws/event-handlers/rider.events');
 const { authMiddleware } = require("./ws/middlewares/auth.ws");
 const { toJSON, stringify } = require("./ws/utils/json");
-const { removeClient } = require('./ws/utils/clients');
+const { removeClient, addClient, clients: wsClients } = require('./ws/utils/clients');
 const wsWrapper = require('./ws/middlewares/wrapper.ws').socketAsyncWrapper;
 
-let client = null;
+const clients = require('./ws/utils/clients').wsClients;
+
+let curr_client = null;
+function startSocketEventListeners(client) {
+    // Init event handlers
+    new RiderSockets(client, wss).init()
+    client.send('Connection established');
+
+    /**
+     * For plain message from curr_client
+     * message - string
+     * 
+     * For event message from curr_client
+     * message - JSON string
+     * message = { event, data }
+     * event - string
+     * data - object
+     */
+    client.on('message', (message) => {
+        curr_client = client;
+        console.log('--------')
+        const parsed_message = toJSON(message);
+
+        // If no event is specified - Assume it's a server message
+        if (parsed_message == null || parsed_message.event == 'ws:message') {
+            // Plain message
+            wss.emit('ws:message', message.toString());
+            return;
+        }
+
+        const { event, data } = parsed_message;   // If event is specified
+
+        client.emit(event, data);   // Emit event
+    });
+
+    client.on('close', () => {
+        curr_client = client;
+        console.log(`${clients[curr_client.user.email]}  disconnected`)
+        removeClient(client);
+    });
+}
+
 wss.on('connection', async (ws, request) => {
     try {
-        client = ws
+        curr_client = ws
         let res = await authMiddleware(ws, request);
         if (res instanceof Error) {
             ws.send('Authentication failed');
             ws.close();
-
             return;
         } else {
             ws = res;
+            addClient(ws);
         }
 
-        // Init event handlers
-        new RiderSockets(ws, wss).init()
-
-        console.log('new client connected')
-        ws.send('Connection established');
-
-        /**
-         * For plain message from client
-         * message - string
-         * 
-         * For event message from client
-         * message - JSON string
-         * message = { event, data }
-         * event - string
-         * data - object
-         */
-        ws.on('message', (message) => {
-            console.log('--------')
-            const parsed_message = toJSON(message);
-            console.log(parsed_message)
-
-            // If no event is specified - Assume it's a sevrer message
-            if (parsed_message == null) {
-                // Plain message
-                wss.emit('ws:message', message.toString());
-
-                // console.log(parsed_message)
-                // wss.emit('error', 'Message is not a valid JSON');
-
-                // // Send error to error handler
-                return;
-            }
-
-            // Server specific message
-            if (parsed_message.event == 'ws:message') {
-                wss.emit('ws:message', parsed_message.data);
-                return;
-            }
-
-            // Socket specific message
-            ws.emit(parsed_message.event, parsed_message.data);
-
-            return;
-        });
-
-        ws.on('close', () => {
-            console.log('Client disconnected');
-            removeClient(ws);
-        });
+        startSocketEventListeners(ws);
     } catch (error) {
         // ws.close();
         wss.emit('error', error)
@@ -85,9 +78,9 @@ wss.on('connection', async (ws, request) => {
 
 // Listen for server messages
 wss.on('ws:message', (message) => {
-    console.log('[msg] frontend: ' + message);
+    console.log(`[msg] ${curr_client.id} : ${message}`);
 
-    client.send(JSON.stringify({
+    curr_client.send(JSON.stringify({
         event: 'backend:message',
         data: "Message received"
     }))
@@ -99,14 +92,14 @@ wss.on('ws:message', (message) => {
  * @todo - Add cases for different error types
  */
 wss.on('error', (error) => {
-    if (client != null && client.readyState == WebSocketServer.OPEN) {
+    if (curr_client != null && curr_client.readyState == WebSocketServer.OPEN) {
         const data = stringify({
             data: "Error occured",
             event: "backend:message"
         })
 
-        client.send(data)
-        // client.close()
+        curr_client.send(data)
+        // curr_client.close()
     }
 
     console.log(error);
