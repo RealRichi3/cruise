d3667ce7cfdebd8ee0bd9b7a339da2bddf5c4e39
Richie <molunorichie@gmail.com>
2023-01-14 02:17:35 +0100

    successful test for sending ride request to available riders - ride.controller.js

--- src/controllers/ride.controller.js
+++ src/controllers/ride.controller.js
@@ -1,12 +1,18 @@
 const { DepartureOrDestination, RiderLocation } = require("../models/location.model");
+const { wsClients, clientIndexes, clients } = require('../ws/utils/clients')
+const { stringify } = require('../utils/json');
+const { BadRequestError } = require('../utils/errors');
+const { Rider } = require('../models/users.model')
+const Ride = require('../models/ride.model')
 
 const bookRide = async (req, res, next) => {
+    // console.log(req.body)
     //    Get the ride info
     const { departure, destination, } = req.body;
 
-    if (!departure || !arrival ||
-        !departure.coordinates || !arrival.coordinates ||
-        !departure.address || !arrival.address) {
+    if (!departure || !destination ||
+        !departure.coordinates || !destination.coordinates ||
+        !departure.address || !destination.address) {
         return next(new BadRequestError('Invalid ride info'));
     }
 
@@ -20,13 +26,16 @@ const bookRide = async (req, res, next) => {
     })
 
     const destination_location = await DepartureOrDestination.create({
-        address: arrival.address,
+        address: destination.address,
         type: 'destination',
         location: {
             type: 'Point',
-            coordinates: arrival.coordinates,
+            coordinates: destination.coordinates,
         },
     })
+
+    // console.log(departure_location)
+    // console.log(destination_location)
 
     //    Check riders within the current users location
     //    Get the nearest rider
@@ -35,17 +44,42 @@ const bookRide = async (req, res, next) => {
             $near: {
                 $geometry: {
                     type: "Point",
-                    coordinates: [departure.coordinates[0], departure.coordinates[1]],
+                    coordinates: departure_location.location.coordinates,
                 },
-                $maxDistance: 1000,
+                $maxDistance: 100000000000000000,
             },
         },
-    }).populate('rider vehicle')
+    }).populate({
+        path: 'rider',
+        populate: {
+            path: 'user',
+        },
+    }).populate('vehicle')
+    function calcCordDistance(cord1, cord2) {
+        const R = 6371e3; // metres
+
+        const φ1 = cord1[0] * Math.PI / 180; // φ, λ in radians
+        const φ2 = cord2[0] * Math.PI / 180;
+        const Δφ = (cord2[0] - cord1[0]) * Math.PI / 180;
+        const Δλ = (cord2[1] - cord1[1]) * Math.PI / 180;
+
+        const a = Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
+            Math.cos(φ1) * Math.cos(φ2) *
+            Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+        const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+        const d = R * c; // in metres
+        return d;
+    }
 
     //   Calculate distance between rider and user
     closest_riders.forEach((rider) => {
-        rider.distance = rider.location.distanceTo(departure_location.location)
+        rider.distance = calcCordDistance(
+            rider.location.coordinates,
+            departure_location.location.coordinates)
     })
+
+    // console.log(closest_riders)
 
     //   Sort riders by distance
     const sorted_riders = closest_riders.sort((a, b) => {
@@ -58,14 +92,99 @@ const bookRide = async (req, res, next) => {
     })
 
     //    Send request to rider,
-        //  Check clients.rider.id in array of socket connections
-        //  If rider is connected, send notification to rider
-        //  Emmit event to rider
-        //  Wait for rider to accept or decline - limit time to 20 seconds
-        //  If rider accepts, create a ride, and init map tracking for rider on user app and rider app
-        //  If rider declines, try next rider - limit to 3 riders
+    function sendRequestToRiders(riders) {
+        return new Promise(async (resolve, reject) => {
+            try {
+                //  Get current client from socket connections
+                // const curr_client = wsClients[riders[0].rider.email]
+                const rider = riders[0].rider
+                const cli_index = clientIndexes[rider.user.email]
+                const curr_client = clients[cli_index]
+
+                console.log(clientIndexes)
+                console.log(cli_index)
+
+                // console.log(clients[0])
+                console.log(Object.keys(clientIndexes))
+                console.log(rider)
+                console.log(curr_client)
+
+                console.log(riders[0].rider.user)
+                const curr_rider = await Rider.findOne({ user: riders[0].rider.user._id })
+
+                //  If rider is connected, send notification to rider
+                if (curr_client) {
+                    curr_client.send('ride:request', stringify({
+                        departure: departure_location,
+                        destination: destination_location,
+                    }))
+                }
+
+                // Wait for rider to accept or decline - limit time to 20 seconds
+                curr_client.on('ride:request_response', (data) => {
+                    if (data.accepted) {
+                        resolve(data)
+                        //  If rider accepts, create a ride, and init map tracking for rider on user app and rider app
+                    } else {
+                        //  If rider declines, try next rider - limit to 3 riders
+                        resolve(null)
+                    }
+                    resolve(data)
+                })
+
+                setTimeout(() => {
+                    //  If no rider accepts, send notification to user
+                    // curr_client.send('ride:request_response', stringify({
+                    //     accepted: false,
+                    //     message: 'No rider available',
+                    // }))
+                    resolve(null)
+                }, 20000)
+            } catch (error) {
+                reject(error)
+            }
+        })
+    }
+
+    const response = await sendRequestToRiders(available_riders)
+    if (!response) {
         //  If no rider accepts, send notification to user
-    
+        return res.status(200).json({
+            success: false,
+            data: {
+                message: 'No rider available',
+            }
+        })
+    }
+
+    console.log(response)
+
+    //  If rider accepts, create a ride, and init map tracking for rider on user app and rider app
+    const ride = await Ride.create({
+        departure: departure_location._id,
+        destination: destination_location._id,
+        rider: response.rider._id,
+        user: req.user._id,
+        vehicle: response.vehicle._id,
+    })
+
+    return res.status(200).json({
+        success: true,
+        data: {
+            message: 'Ride request sent',
+        }
+    })
+    //  Get current client from socket connections
+
+
+
+
+
+    //  If rider is not connected, send notification to rider
+
+    //  Emmit event to rider
+    //  Wait for rider to accept or decline - limit time to 20 seconds
+
 };
 
 const acceptRideRequest = async (req, res, next) => { };
