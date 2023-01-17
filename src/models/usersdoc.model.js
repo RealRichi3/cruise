3cc6cf1492fa4b3be25f61cf8bedc8e57ab7b920
Emmanuel Umukoro <emmanuel.umukoro22@gmail.com>
2023-01-17 19:29:32 +0100

    Vehicle file upload configuration final state

--- src/models/usersdoc.model.js
+++ src/models/usersdoc.model.js
@@ -1,38 +1,28 @@
 const { default: mongoose } = require('mongoose');
 const schema = mongoose.Schema;
 
-const uploadStatusSchema = new schema(
-    {
-        user: { type: schema.Types.ObjectId, ref: 'User', required: true },
-        isVerified: { type: Boolean, default: false },
-        isUploaded: { type: Boolean, default: false },
-    },
-    { timestamps: true }
-);
 
 const profileuploadSchema = new schema(
     {
-        user: { type: schema.Types.ObjectId, ref: 'User', required: true },
+        enduser: { type: schema.Types.ObjectId, ref: 'EndUser'},
+        rider: { type: schema.Types.ObjectId, ref: 'Rider'},
         image: { type: String, required: true },
     },
     { timestamps: true }
 );
 
-const userdocSchema = new schema(
+const riderdocSchema = new schema(
     {
-        user: { type: schema.Types.ObjectId, ref: 'User', required: true },
+        rider: { type: schema.Types.ObjectId, ref: 'Rider', required: true },
         docurl: { type: String, required: true },
         docname: { type: String, required: true },
-        docnumber: { type: String, required: true },
-        docexpiry: { type: String, required: true },
-        docstatus: { type: String, required: true },
     },
     { timestamps: true }
 );
 
 const vehicleimagesSchema = new schema(
     {
-        user: { type: schema.Types.ObjectId, ref: 'User', required: true },
+        rider: { type: schema.Types.ObjectId, ref: 'Rider', required: true },
         vehicle: { type: schema.Types.ObjectId, ref: 'Vehicle', required: true },
         imagearray: { type: Array, required: true },
     },
@@ -41,7 +31,7 @@ const vehicleimagesSchema = new schema(
 
 const vehicledocsSchema = new schema(
     {
-        user: { type: schema.Types.ObjectId, ref: 'User', required: true },
+        rider: { type: schema.Types.ObjectId, ref: 'Rider', required: true },
         vehicle: { type: schema.Types.ObjectId, ref: 'Vehicle', required: true },
         docArray: { type: Array, required: true },
     },
@@ -84,16 +74,14 @@ userdocSchema.pre('save', function (next) {
 // });
 
 
-const UploadStatus = mongoose.model('UploadStatus', uploadStatusSchema);
 const ProfileUpload = mongoose.model('ProfileUpload', profileuploadSchema);
-const UserDoc = mongoose.model('UserDoc', userdocSchema);
+const RiderDoc = mongoose.model('UserDoc', riderdocSchema);
 const VehicleImages = mongoose.model('VehicleImages', vehicleimagesSchema);
 const VehicleDocs = mongoose.model('VehicleDocs', vehicledocsSchema);
 
 module.exports = {  
-    UploadStatus,
     ProfileUpload,
-    UserDoc,
+    RiderDoc,
     VehicleImages,
     VehicleDocs
 };
