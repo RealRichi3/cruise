f1f376e8cc40358241726edc995b828f288f783d
Richie <molunorichie@gmail.com>
2023-01-19 05:18:45 +0100

    add: createNewRide shcema method for rideRequestSchema - ride.model.js

--- src/models/ride.model.js
+++ src/models/ride.model.js
@@ -1,20 +1,24 @@
 const mongoose = require('mongoose')
 const schema = mongoose.Schema
+const { Rider } = require('./users.model')
 
 const rideRequestSchema = new schema({
+    user: { type: schema.Types.ObjectId, ref: 'User', required: true },
+    departure: { type: schema.Types.ObjectId, ref: 'DepartureOrDestination', required: true },
+    destination: { type: schema.Types.ObjectId, ref: 'DepartureOrDestination', required: true },
+    payment_method: { type: String, enum: ['cash', 'card', 'wallet'] },
+    status: { type: String, enum: ['pending', 'accepted', 'cancelled'], default: 'pending' },
     urban_cost: { type: Number },
     standard_cost: { type: Number },
     elite_cost: { type: Number },
     ride_class: { type: String, enum: ['urban', 'standard', 'elite'] },
-    departure: { type: schema.Types.ObjectId, ref: 'DepartureOrDestination', required: true },
-    destination: { type: schema.Types.ObjectId, ref: 'DepartureOrDestination', required: true },
     distance: { type: Number },  // in kilometers
-    user: { type: schema.Types.ObjectId, ref: 'User', required: true },
-    ride: { type: schema.Types.ObjectId, ref: 'Ride'},
-    payment_method: { type: String, enum: ['cash', 'card', 'wallet'] },
-    status: { type: String, enum: ['pending', 'accepted', 'cancelled'], default: 'pending' },
+    ride: { type: schema.Types.ObjectId, ref: 'Ride' },
 })
 
+/**
+ * @todo: Implement getEstimatedRideTime() method
+ */
 const rideSchema = new schema({
     rider: { type: schema.Types.ObjectId, ref: 'Rider', required: true },
     passenger: {
@@ -36,6 +40,69 @@ const rideSchema = new schema({
     },
     createdAt: { type: Date, default: Date.now },
 })
+
+rideSchema.virtual('ride_request', {
+    ref: 'RideRequest',
+    localField: '_id',
+    foreignField: 'ride',
+})
+
+/**
+ * 
+ * @param {string} rider_id 
+ * @returns {Promise<Ride>}
+ * 
+ * @todo 
+ */
+// TODO: set ride status to 'accepted' after ride is created
+rideRequestSchema.methods.createNewRide = async function (rider_id) {
+    try {
+        const rider = await Rider.findById(rider_id)
+        if (!rider) throw new Error('Rider not found')
+        
+        // Create new ride
+        const ride = await Ride.create({
+            rider: rider._id,
+            passenger: this.user,
+            vehicle: rider.currentVehicle,
+            departure: this.departure,
+            destination: this.destination,
+        })
+        
+        // Update ride request to include ride
+        this.ride = ride._id
+
+        // Populate ride
+        ride.populate({
+            path: 'passenger',
+            select: 'firstname lastname',
+            populate: {
+                path: 'enduser',
+                select: 'phone -user'
+            }
+        }).catch(err => console.log(err))
+        ride.populate('vehicle')
+
+        // Update ride request status
+        this.status = 'accepted'
+        await this.save()
+        // console.log(ride)
+    
+        return ride
+            .populate({
+                path: 'rider',
+                select: 'phone address city state _id',
+                populate: {
+                    path: 'user',
+                    select: 'firstname lastname'
+                },
+            })
+        
+    } catch (error) {
+        console.log(error)
+        return error
+    }
+}
 
 const Ride = mongoose.model('Ride', rideSchema)
 const RideRequest = mongoose.model('RideRequest', rideRequestSchema)
