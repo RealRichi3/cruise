bec3758620e191608d6b9995d183ca8db5642f8d
Richie <molunorichie@gmail.com>
2023-02-10 23:21:45 +0100

    complete location setup

--- src/ws/event-handlers/location.events.js
+++ src/ws/event-handlers/location.events.js
@@ -4,11 +4,10 @@ const saveNewLocation = async function (data, socket) {
     try {
         if (!socket) socket = this;
 
-        // console.log(socket.user)
         const { location } = data
-        console.log(location)
         const [longitude, latitude] = location.coordinates
 
+        // Create new location
         const new_location = await RiderLocation.create({
             rider: socket.user.rider._id,
             location: {
@@ -17,62 +16,62 @@ const saveNewLocation = async function (data, socket) {
             },
         });
 
-        console.log(new_location)
-
         return new_location;
     } catch (error) {
         console.log(error)
     }
 };
 
-const updateLocation = async function (data) {
+const updateLocation = async function (data, res) {
     try {
         const socket = this
-        console.log('Updating location', data)
         const curr_location = data.location
         const [longitude, latitude] = curr_location.coordinates
-        // console.log(socket.user.rider)
         const location = await RiderLocation.findById(socket.user.rider.location);
-        console.log(location)
 
         if (!location) {
             // Create new location
-            const new_location_data = { rider_id: socket.user.rider._id, location: { coordinates: [longitude, latitude] } }
-
+            const new_location_data = {
+                rider_id: socket.user.rider._id,
+                location: { coordinates: [longitude, latitude] }
+            }
             await saveNewLocation(new_location_data, socket)
-            console.log('New location', new_location_data)
 
-            return new_location_data
+            res(null, new_location_data)
         }
 
-        const new_location_data = await location.updateCoordinates(long, lat)
-        console.log('New location data', new_location_data)
+        const new_location_data = await location.updateCoordinates(longitude, latitude)
 
-        return new_location_data
+        res(null, new_location_data)
     } catch (error) {
         console.log(error)
+        res(error)
     }
 }
 
-const getLocation = async (vehicleId) => {
-    const location = await RiderLocation.findOne({ vehicle: vehicleId })
+const getLocation = async function (data, res) {
+    const socket = this
 
-    if (!location) { return null; }
+    const { rider } = socket.user
+    const rider_location = rider.location
 
-    return location
-}
+    if (!rider_location) {
+        res('Rider location not found', null)
+    }
 
-const deleteVehicleLocation = async (vehicle_id) => {
-    const location = await RiderLocation.findOneAndDelete({ vehicle: vehicle_id })
-
-    if (!location) { return null; }
-
-    return location
+    res(null, rider_location)
 }
 
 module.exports = (io, socket) => {
-    socket.on('update-location', updateLocation);
-    socket.on('get-location', getLocation);
-    socket.on('save-new-location', saveNewLocation);
-    socket.on('delete-vehicle-location', deleteVehicleLocation)
+    const res = (err, data) => {
+        if (err) {
+            socket.emit('error', err)
+        } else {
+            socket.emit('success', data)
+        }
+    }
+
+    socket.on('update-location', (data) => updateLocation.call(socket, data, res));
+    socket.on('get-location', (data) => getLocation.call(socket, data, res), res);
+    socket.on('save-new-location', (data) => saveNewLocation.call(socket, data, res));
 }
